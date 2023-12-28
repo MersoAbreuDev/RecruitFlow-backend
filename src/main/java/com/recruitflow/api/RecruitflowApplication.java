4507970cd7fea51124ab83edfe343cc9a0835587
EmersonAEL <emerson@aelconsultoriasistemas.com.br>
2023-12-28 14:12:38 -0300

    Iniciando projeto

---
+++ src/main/java/com/recruitflow/api/RecruitflowApplication.java
@@ -0,0 +1,13 @@
+package com.recruitflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruitflow/api/RecruitflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitflow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
